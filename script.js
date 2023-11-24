8ca0817fca4029ae6f0d15ee5b81e8f59438a9dd
vladis112233 <124692818+vladis112233@users.noreply.github.com>
2023-11-24 16:56:39 +0200

    22

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
 }
+
+var links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        var targetId = this.getAttribute('href').substring(1);
+        var targetElement = document.getElementById(targetId);
+
+        var offsetTop = targetElement.offsetTop;
+
+        window.scrollTo({
+            top: offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
